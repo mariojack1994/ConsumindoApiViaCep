3e8aa0ff1f63569d6f947ea535062510071bc242
Mário Gonçalves <mario.jhowemack1994@gmail.com>
2022-05-25 06:59:41 -0300

    Primeiro commit

---
+++ CotacaoDolarAPI/DadosCotacao.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CotacaoDolarAPI
+{
+    public class DadosCotacao
+    {
+        [JsonProperty("@odata.context")]
+        public string OdataContext { get; set; }
+        public List<Value> value { get; set; }
+    }
+
+    public class Value
+    {
+        public double cotacaoCompra { get; set; }
+        public double cotacaoVenda { get; set; }
+        public string dataHoraCotacao { get; set; }
+    }
+
+}
---
+++ CotacaoDolarAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace CotacaoDolarAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = string.Empty;
+
+            HttpClient cliente = new HttpClient();
+
+            Console.WriteLine("INFORME A DATA NO FORMATO MM-DD-YYYY");
+            data = Console.ReadLine();
+
+            HttpResponseMessage retornoHttp;
+
+            retornoHttp = cliente.GetAsync($"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27{data}%27&$top=100&$format=json").Result;
+
+            string respostaString = retornoHttp.Content.ReadAsStringAsync().Result;
+            DadosCotacao dados = JsonConvert.DeserializeObject<DadosCotacao>(respostaString);
+
+            Console.WriteLine($" COTAÇÃO INICIO :{dados.value[0].cotacaoCompra}");
+            Console.WriteLine($" COTAÇÃO FIM :{dados.value[0].cotacaoVenda}");
+            Console.WriteLine($"DATA DA COTAÇÃO :{dados.value[0].dataHoraCotacao}");
+
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
